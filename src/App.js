5bba06b96b04f0ab2988e8cb023e36b5419a1579
Muhammed Nibras <mohdnibras1998@gmail.com>
2022-11-23 16:21:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import img from './contact.webp'
 
 function App() {
+  const [user, setUsers] = useState(null)
+  const [reveal, setReveal] = useState(false)
+  const [selected,setSelected] = useState(null)
+  useEffect(() => {
+    axios.get("https://api.mockaroo.com/api/c31c9cf0?count=10&key=6aac9b50")
+      .then(res =>
+        setUsers(res.data))
+  }, [])
+  
+
+  const toggleage = (item) => {
+ setSelected(item)
+ 
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      {user && user.map((item) => {
+        return (<>
+          <div className='App'><div id="img"><img id="propic" src={img} /></div>
+            <div id="details">
+              <p>Name:{item.name}</p>
+              <p>Email:{item.email}</p>
+              <button onClick={()=>toggleage(item)}>Toggle Age</button>
+              <h3>{selected==item?item.age:null}</h3>
+            </div></div>
+        </>
+
+        )
+      })
+
+
+      }
+
+
     </div>
   );
 }
